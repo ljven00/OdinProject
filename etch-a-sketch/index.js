386c942d4ebf9b34fd76317e5512287c79dc1131
ljven00 <jeanvenaldolouissaint@gmail.com>
2023-06-24 21:45:31 -0600

    Create boiler playe files

---
+++ etch-a-sketch/index.js
@@ -0,0 +1,28 @@
+
+let mouseDown = false;
+
+window.addEventListener("mousedown", () => {
+    mouseDown = true;
+});
+
+window.addEventListener("mouseup", () => {
+    mouseDown = false;
+});
+
+/**
+ * @returns a random color in rgba form
+ */
+function randomColor() {
+    let red = getRand();
+    let green = getRand();
+    let blue = getRand();
+    let alpha = Math.random().toFixed(2);
+    return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+}
+
+/**
+ * @returns an integer between 0 and 255
+ */
+function getRand() {
+    return Math.floor(Math.random() * 256);
+}
